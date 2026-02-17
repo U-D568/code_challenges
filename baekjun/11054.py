a2e978cf2fae4c48f43de6c01c178043b933080b
d568 <test@test.com>
2026-02-18 01:49:58 +0900

    commit

--- baekjun/11054.py
+++ baekjun/11054.py
@@ -8,29 +8,24 @@ def read_ints() -> List[int]:
 
 def main():
     N = read_ints()[0]
-    ary = read_ints()
+    ary = [0]
+    ary.extend(read_ints())
 
     ascending = [0] * (N + 1)
-    for i in range(0, N):
-        while not (stack[-1] < ary[i]):
-            stack.pop()
-        stack.append(ary[i])
-        ascending[i] = len(stack) - 1
-    print(ascending)
+    for i in range(1, N + 1):
+        for j in range(i):
+            if ary[j] < ary[i]:
+                ascending[i] = max(ascending[i], ascending[j] + 1)
 
-    descending = [0] * N
-    stack = [0]
-    for i in range(N - 1, -1, -1):
-        while not (stack[-1] < ary[i]):
-            stack.pop()
-        stack.append(ary[i])
-        descending[i] = len(stack) - 1
-    print(descending)
+    descending = [0] * (N + 1)
+    for i in range(N, 0, -1):
+        for j in range(N, i, -1):
+            if ary[j] < ary[i]:
+                descending[i] = max(descending[i], descending[j] + 1)
 
-    total = [-1] * N
+    total = [0] * N
     for i in range(N):
-        total[i] = ascending[i] + descending[i] - 1
-    print(total)
+        total[i] = ascending[i + 1] + descending[i + 1]
     print(max(total))
     
 
--- baekjun/1107.py
+++ baekjun/1107.py
@@ -1,24 +1,63 @@
 import sys
-import itertools
-
-chanel = int(sys.stdin.readline())
-num_broken = int(sys.stdin.readline())
-if num_broken != 0:
-    broken = list(map(int, sys.stdin.readline().split()))
-
-not_broken = []
-for i in range(10):
-    if i in broken:
-        continue
-    not_broken.append(i)
-not_broken.sort()
-
-def permutation(numbers):
-    result = itertools.combinations(numbers)
-    print(result)
+from typing import List
+import math
 
 
-def search(length, numbers):
-    pass
+def read_ints() -> List[int]:
+    return list(map(int, sys.stdin.readline().split()))
 
-permutation(list(range(10)))
+
+def make_number(depth, max_depth, numbers, values):
+    if max_depth == depth:
+        return values
+
+    next_values = []
+    for num in numbers:
+        for v in values:
+            new_val = v * 10 + num
+            if new_val > 1000000:
+                continue
+            next_values.append(new_val)
+    return make_number(depth+1, max_depth, numbers, next_values)
+
+
+
+def main():
+    target = read_ints()[0]
+    N = read_ints()[0]
+    not_use = read_ints() if N != 0 else []
+    buttons = []
+    for i in range(10):
+        if i in not_use:
+            continue
+        buttons.append(i)
+
+    from_init = abs(target - 100)
+
+    length = len(str(target))
+    candidates = []
+    if length > 1:
+        res = make_number(0, length - 1, buttons, [0])
+        candidates.extend(res)
+        res = make_number(length - 1, length, buttons, res)
+        candidates.extend(res)
+    else:
+        res = list(buttons)
+        candidates.extend(buttons)
+    res = make_number(length, length + 1, buttons, res)
+    candidates.extend(res)
+
+    counts = [from_init]
+    min_diff = math.inf
+    min_val = -1
+    for channel in candidates:
+        diff = abs(target - channel)
+        if diff < min_diff:
+            min_diff = diff
+            min_val = channel
+        counts.append(len(str(channel)) + abs(target - channel))
+    print(min(counts))
+
+
+if __name__ == "__main__":
+    main()
