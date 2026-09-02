9fef337dc73de64fffe0f2026c2ab709b2451886
d568 <test@test.com>
2026-09-02 22:39:44 +0900

    c

---
+++ swea/4070.py
@@ -0,0 +1,11 @@
+T = int(input())
+dp = [0] * 251
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3, 251):
+    dp[i] = dp[i-1] + 2 * dp[i-2]
+
+for tc in range(1, T + 1):
+    N = int(input())
+    print(f"#{tc} {dp[N]}")
